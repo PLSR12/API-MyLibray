11e4179e5248df8e352c743a719f2e45e49eab93
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2024-02-03 18:50:23 -0300

    change url dev envs

--- src/config/database.js
+++ src/config/database.js
@@ -4,24 +4,21 @@ dotenv.config();
 
 module.exports = {
 	development: {
+		url: "postgres://library_xldd_user:l3eOdmglPNYo57Ut52z5dqkVXH2vN8kW@dpg-cms2fu0l5elc73erm4j0-a/library_xldd",
+		dialect: "postgres",
+	},
+	test: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
-	test: {
-		username: "root",
-		password: null,
-		database: "database_test",
-		host: "127.0.0.1",
-		dialect: "postgres",
-	},
 	production: {
-		username: "root",
-		password: null,
-		database: "database_production",
-		host: "127.0.0.1",
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
 };
