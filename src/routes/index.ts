3864fd8a2c5b431e4fbf6a3f5a88a449c645e739
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2024-02-05 21:05:46 -0300

    feature: add middleware for auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Application } from "express";
+import { Application, Router } from "express";
 import bodyParser from "body-parser";
 import auth from "./authRoutes";
 import customers from "./customersRoutes";
@@ -6,12 +6,17 @@ import categories from "./categoriesRoutes";
 import publishingCompanies from "./publishingCompanies";
 import books from "./booksRoutes";
 import orders from "./ordersRoute";
+import authMiddleware from "../middlewares/auth";
 
 export default (app: Application) => {
+	const router = Router();
+	const middlewareAuth = router.use(authMiddleware);
+
 	app.use(
 		bodyParser.json(),
 		auth,
 		customers,
+		middlewareAuth,
 		categories,
 		publishingCompanies,
 		books,
